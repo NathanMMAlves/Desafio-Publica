a8a57d34513b8a2a880af29c47aaf2e6c490dd32
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-01 00:22:49 -0300

    Mapeação das informações para o BD

--- DesafioPublica/DesafioCore/DB/Map/JogoPlacarMap.cs
+++ DesafioPublica/DesafioCore/DB/Map/JogoPlacarMap.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using DesafioCore.DB.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DesafioCore.DB.Map
 {
-    public class JogoPlacarMap
+    public class JogoPlacarMap : IDataBaseMap
     {
+        //Mapeação das informações para o BD
+        public void Map(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JogoPlacar>().HasKey(x => x.Id);
+            modelBuilder.Entity<JogoPlacar>().Property(x => x.Placar).IsRequired();
+            modelBuilder.Entity<JogoPlacar>().Property(x => x.MaximoDaTemporada).IsRequired();
+            modelBuilder.Entity<JogoPlacar>().Property(x => x.MinimoDaTemporada).IsRequired();
+            modelBuilder.Entity<JogoPlacar>().Property(x => x.QuebraRecordeMin).IsRequired();
+            modelBuilder.Entity<JogoPlacar>().Property(x => x.QuebreRecordeMax).IsRequired();
+        }
     }
 }
