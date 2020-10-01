84b04cac9e2bb0872d35837bf7cc4f47b410e50d
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-01 00:41:40 -0300

    Inicializador para o BD

---
+++ DesafioPublica/DesafioCore/DB/InicializadorBancoDeDados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DesafioCore.DB
+{
+
+    //É por aqui que o BD sera inicializado.
+    //Caso não haja banco criado, ele irá criar um automaticamente.
+    public class InicializadorBancoDeDados
+    {
+        private IManipuladorBancoDeDados manipuladorBancoDeDados;
+
+        public InicializadorBancoDeDados(IManipuladorBancoDeDados manipuladorBancoDeDados)
+        {
+            this.manipuladorBancoDeDados = manipuladorBancoDeDados;
+        }
+
+        public void Iniciar()
+        {
+            if (!File.Exists(Constantes.NomeArquivoBanco))
+                manipuladorBancoDeDados.CriarNovo();
+        }
+    }
+}
