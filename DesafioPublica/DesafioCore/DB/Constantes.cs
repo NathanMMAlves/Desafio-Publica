8dc2fde815036a28f3c9b7b67314add21719b20e
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-01 00:34:50 -0300

    Manipulador do Sqlite

---
+++ DesafioPublica/DesafioCore/DB/Constantes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioCore.DB
+{
+    public class Constantes
+    {
+        public const string NomeArquivoBanco = "placar.db";
+    }
+}
---
+++ DesafioPublica/DesafioCore/DB/ManipuladorSqLite.cs
@@ -0,0 +1,36 @@
+﻿using DesafioCore.DB.Map;
+using DesafioCore.DB.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioCore.DB
+{
+
+    //É aqui que sera feito o registro no BD de todos os jogos cadastrados
+    public class ManipuladorSqLite : DbContext, IManipuladorBancoDeDados
+    {
+        private DbSet<JogoPlacar> JogosPlacares { get; set; }
+        public void AdicionarPlacar(JogoPlacar jogoPlacar)
+        {
+            JogosPlacares.Add(jogoPlacar);
+            SaveChanges();
+        }
+
+        public void CriarNovo()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=" + Constantes.NomeArquivoBanco);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            new JogoPlacarMap().Map(modelBuilder);
+        }
+    }
+}
