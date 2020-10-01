cd4cd5740837590aa1d42b51f76aa9bcbe96ec42
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-01 00:25:26 -0300

    Interface para o Manipulação do BD

---
+++ DesafioPublica/DesafioCore/DB/IManipuladorBancoDeDados.cs
@@ -0,0 +1,14 @@
+﻿using DesafioCore.DB.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioCore.DB
+{
+    //Interface para o Manipulação do BD
+    public interface IManipuladorBancoDeDados
+    {
+        void CriarNovo();
+        void AdicionarPlacar(JogoPlacar jogoPlacar);
+    }
+}
