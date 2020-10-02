114ec815f2b008a6f2833ccdbfb2e127dd81c82d
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-02 10:59:41 -0300

    Adicionado funcionalidade para inicializar banco de dados no startup

--- DesafioPublica/DesafioWeb/Startup.cs
+++ DesafioPublica/DesafioWeb/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DesafioCore.DB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -16,17 +17,18 @@ namespace DesafioWeb
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            var inicializador = new InicializadorBancoDeDados(new ManipuladorSqLite());
+            inicializador.Iniciar();
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddScoped(typeof(IManipuladorBancoDeDados), typeof(ManipuladorSqLite));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +38,6 @@ namespace DesafioWeb
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
