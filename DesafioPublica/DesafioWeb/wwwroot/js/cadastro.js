3e70b6d8c7152c8c0ae4bc951abfc5fd138a0dff
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-04 15:37:25 -0300

    Funções para salvar usando ajax e javascript

--- DesafioPublica/DesafioWeb/wwwroot/js/cadastro.js
+++ DesafioPublica/DesafioWeb/wwwroot/js/cadastro.js
@@ -3,14 +3,63 @@
         $('form')[0].reset();
     }
 
+    static eValidoPlacar(strPlacar) {
+        if (strPlacar < 0 || strPlacar > 999) return false;
+    }
     static pontuacaoFormat(evt) {
         var numero = evt.target.value.replace(/\D/g, "");
-        if (numero < 3 || numero > 999) {
+        if (numero < 3 || numero > 999 || !Cadastro.eValidoPlacar(numero)) {
             $(evt.target).addClass("invalid");
         } else {
             $(evt.target).removeClass("invalid");
         }
         evt.target.value = numero;
-        return numeroPlacar = numero;
+        return numeroPontuacao = numero;
+    }
+
+    static salvarCadastro() {
+        if (!Cadastro.formularioCorreto()) {
+            alert("Favor verificar as informações preenchidas.");
+            return;
+        }
+        var cadastro = Cadastro.obterValores();
+        $('form :input').prop("disabled", true);
+
+        var sucess = (msg) => {
+            $('form :input').prop("disabled", false);
+            if (msg) {
+                alert(msg);
+                return;
+            }
+
+            alert('Salvo');
+            Cadastro.novoCadastro();
+        }
+        var fail = () => {
+            $('form :input').prop("disabled", false);
+            alert('Houve um erro no servidor, não foi possível salvar.');
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "Cadastrar",
+            data: JSON.stringify(cadastro),
+            contentType: "application/json; charset=utf-8",
+            dataType: "JSON",
+
+            success: function (output) {
+                sucess(output);
+            },
+            error: function () {
+                fail();
+            }
+        });
+    }
+
+    static formularioCorreto() {
+        if (!$("#Pontuacao").val() || $("#Pontuacao").hasClass('invalid')) {
+            $("#Pontuacao").addClass('invalid');
+            return false;
+        }
     }
 }
