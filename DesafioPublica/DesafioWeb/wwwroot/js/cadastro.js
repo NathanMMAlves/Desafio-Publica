7828902a8e68dad9e8b55ddf793df074ccc2f419
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-04 15:07:21 -0300

    Feito evento que só permite a entrada de números no input

---
+++ DesafioPublica/DesafioWeb/wwwroot/js/cadastro.js
@@ -0,0 +1,16 @@
+﻿class Cadastro {
+    static novoCadastro() {
+        $('form')[0].reset();
+    }
+
+    static pontuacaoFormat(evt) {
+        var numero = evt.target.value.replace(/\D/g, "");
+        if (numero < 3 || numero > 999) {
+            $(evt.target).addClass("invalid");
+        } else {
+            $(evt.target).removeClass("invalid");
+        }
+        evt.target.value = numero;
+        return numeroPlacar = numero;
+    }
+}
