67bd4f7e1bff9bc95ba75819b9a1b840f33a4d52
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-04 03:19:13 -0300

    Validador Placar

--- DesafioPublica/DesafioCore/RegraDeNegocio/Validacoes/JogoPlacar/ValidadorPlacar.cs
+++ DesafioPublica/DesafioCore/RegraDeNegocio/Validacoes/JogoPlacar/ValidadorPlacar.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace DesafioCore.RegraDeNegocio.Validacoes.JogoPlacar
 {
-    class ValidadorPlacar
+    public class ValidadorPlacar : IValidacao<DB.Model.JogoPlacar>
     {
+        public ResultadoValidacao Validar(DB.Model.JogoPlacar model)
+        {
+            var placar = model.Placar;
+
+            if (placar < 0 || placar > 1000)
+                return Invalido();
+            else
+                return Valido();
+        }
+        private ResultadoValidacao Valido()
+        {
+            return new ResultadoValidacao { Valido = true };
+        }
+
+        private ResultadoValidacao Invalido()
+        {
+            return new ResultadoValidacao
+            {
+                Valido = false,
+                Mensagem = "Esse placar é inválido!"
+            };
+        }
     }
 }
