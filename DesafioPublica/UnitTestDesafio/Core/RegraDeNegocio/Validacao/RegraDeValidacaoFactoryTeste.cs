1408259e4fd89cc0252aa3e763171051b83c04a1
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-04 04:34:10 -0300

    Feito validador para RegraDeValidaçãoFactory

---
+++ DesafioPublica/UnitTestDesafio/Core/RegraDeNegocio/Validacao/RegraDeValidacaoFactoryTeste.cs
@@ -0,0 +1,26 @@
+﻿using DesafioCore.RegraDeNegocio.Validacoes;
+using DesafioCore.RegraDeNegocio.Validacoes.JogoPlacar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestDesafio.Core.RegraDeNegocio.Validacao
+{
+    [TestClass]
+    public class RegraDeValidacaoFactoryTeste
+    {
+        [TestMethod]
+        public void iniciado_o_programa_deve_ter_os_validadores_para_cadastro()
+        {
+            var factory = new RegrasDeValidacaoFactory();
+            var validadores = factory.ObterValidadoresJogoPlacar();
+            Assert.AreEqual(validadores.Count(v => v.GetType() == typeof(PlacarValidador)), 1);
+            Assert.AreEqual(validadores.Count(v => v.GetType() == typeof(MinimoDaTemporadaValidador)), 1);
+            Assert.AreEqual(validadores.Count(v => v.GetType() == typeof(MaximoDaTemporadaValidador)), 1);
+            Assert.AreEqual(validadores.Count(v => v.GetType() == typeof(QuebraRecordeMinValidador)), 1);
+            Assert.AreEqual(validadores.Count(v => v.GetType() == typeof(QuebreRecordeMaxValidador)), 1);
+        }
+    }
+}
