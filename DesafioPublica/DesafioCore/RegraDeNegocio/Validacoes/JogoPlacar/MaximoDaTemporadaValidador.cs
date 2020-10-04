a54078a76603748e81eed268c6ffcf577602c853
NathanMMAlves <50925454+NathanMMAlves@users.noreply.github.com>
2020-10-04 03:24:12 -0300

    Validadores Minimo e Maximo da Temporada

--- DesafioPublica/DesafioCore/RegraDeNegocio/Validacoes/JogoPlacar/MaximoDaTemporadaValidador.cs
+++ DesafioPublica/DesafioCore/RegraDeNegocio/Validacoes/JogoPlacar/MaximoDaTemporadaValidador.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace DesafioCore.RegraDeNegocio.Validacoes.JogoPlacar
 {
-    class MaximoDaTemporadaValidador
+    public class MaximoDaTemporadaValidador : IValidacao<DB.Model.JogoPlacar>
     {
+        public ResultadoValidacao Validar(DB.Model.JogoPlacar model)
+        {
+            var maximo = model.MaximoDaTemporada;
+
+            if (maximo < 0 || maximo > 999)
+                return Invalido();
+            else
+                return Valido();
+        }
+        private ResultadoValidacao Valido()
+        {
+            return new ResultadoValidacao { Valido = true };
+        }
+
+        private ResultadoValidacao Invalido()
+        {
+            return new ResultadoValidacao
+            {
+                Valido = false,
+                Mensagem = "Houve inválides no máximo da temporada."
+            };
+        }
     }
 }
--- DesafioPublica/DesafioCore/RegraDeNegocio/Validacoes/JogoPlacar/MinimoDaTemporadaValidador.cs
+++ DesafioPublica/DesafioCore/RegraDeNegocio/Validacoes/JogoPlacar/MinimoDaTemporadaValidador.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace DesafioCore.RegraDeNegocio.Validacoes.JogoPlacar
 {
-    class MinimoDaTemporadaValidador
+    public class MinimoDaTemporadaValidador : IValidacao<DB.Model.JogoPlacar>
     {
+        public ResultadoValidacao Validar(DB.Model.JogoPlacar model)
+        {
+            var minimo = model.MinimoDaTemporada;
+
+            if (minimo < 0 || minimo > 999)
+                return Invalido();
+            else
+                return Valido();
+        }
+        private ResultadoValidacao Valido()
+        {
+            return new ResultadoValidacao { Valido = true };
+        }
+
+        private ResultadoValidacao Invalido()
+        {
+            return new ResultadoValidacao
+            {
+                Valido = false,
+                Mensagem = "Houve inválides no mínimo da temporada."
+            };
+        }
     }
 }
